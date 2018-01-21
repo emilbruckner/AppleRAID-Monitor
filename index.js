3f008fb4135799ac31f7baf575a0a5b7124616b3
Emil Bruckner <emil.bruckner@gmail.com>
2018-01-21 08:38:13 +0100

    send error via telegram

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const notifier = require('node-notifier')
 const exec = require('child_process').exec
+const axios = require('axios')
 
 const occurrences = text => (text.match(/Online/g) || []).length
 
@@ -7,9 +8,21 @@ const SHOULD_NUMBER_OF_ONLINE_DRIVES = 9
 
 exec("/usr/sbin/diskutil appleRAID list", function(err, stdout, stderr) {
   if (occurrences(stdout) != SHOULD_NUMBER_OF_ONLINE_DRIVES) {
+    const message = 'Something is wrong with the drives! ' + stdout
     notifier.notify({
       title: 'RAID Alert',
-      message: 'Something is wrong with the drives',
+      message,
     })
+    sendToEmil(message)
   }
 })
+
+function sendToEmil(text) {
+	axios.post("https://postman.timo.sh/TELEGRAM_ID/", text)
+		.then(function (response) {
+		    console.log(response);
+		})
+		.catch(function (response) {
+		    console.log(response);
+		});
+}
